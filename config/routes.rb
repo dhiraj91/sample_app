60cab9ac7b1c8b963d7355fcb56bee7ff17f9699
Dhiraj <dheeraj.dc.91@gmail.com>
2015-03-27 11:46:59 +0530

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SampleApp::Application.routes.draw do
+ 
+ root "pages#show"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
