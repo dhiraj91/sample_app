d463d6474bdf4367f9da0b37fa7fe9f0e3eee338
Dhiraj <dheeraj.dc.91@gmail.com>
2015-03-27 13:17:59 +0530

    commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
 
 def show
-
+@tile = "hi "
 end
+
+def display
+	
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
  
  root "pages#show"
-  
+
+ get "pages/show" => "pages#show"
+ 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
